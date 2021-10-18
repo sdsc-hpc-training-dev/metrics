56c3ea3bd9e4e04b0cb4ee8e25e4a64122665e38
Yuu6883 <okumura6883@gmail.com>
2021-10-18 15:24:57 -0700

    Updated time format

--- assets/index.js
+++ assets/index.js
@@ -76,6 +76,17 @@ $(document).ready(async () => {
                             type: 'time',
                             time: {
                                 tooltipFormat: 'll',
+                                displayFormats: {
+                                    millisecond: 'MMM DD',
+                                    second: 'MMM DD',
+                                    minute: 'MMM DD',
+                                    hour: 'MMM DD',
+                                    day: 'MMM DD',
+                                    week: 'MMM DD',
+                                    month: 'MMM DD',
+                                    quarter: 'MMM DD',
+                                    year: 'MMM DD',
+                                },
                             },
                         },
                     ],
